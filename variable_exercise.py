f24403d4e248057b6b72f0375e387ca8a025ead0
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-02 15:44:56 +0000

    Create variable_exercise.py

---
+++ variable_exercise.py
@@ -0,0 +1,12 @@
+# Create 3 variables to get user data
+
+# name 
+name = input('Please enter your full name')
+# DOB 
+dob = input('Please enter your date of birth in the format DD/MM/YYYY')
+
+# age
+age = input('Please enter your age')
+
+# display all the information
+print('Name : {}, DOB : {}, Age : {}'.format(name, dob, age))
