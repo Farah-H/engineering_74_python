dde0457272e0fbb1d45c8518a158b367d581a9cd
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-02 15:06:56 +0000

    print and type commands in python added

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Why Python
+## Pycharm Setup 
+### Python 3.7
+### Python Variable
+
+- What is a variable
+- Dynamically typed languages
+- Overwriting variables 
+- Getting user input from the command line
+
+
+# Variable:
+- Is a placeholder to store data
+- Essentially it is a box
+- beauty of IDE, when you declare a variable you can press tab, no need to fully type it every time
+
+# pseudo coding 
+- basically it is a translation of your code 
+- It is important because not everyone you work with will be technical, if you show them just the code they may not understand it 
---
+++ python_variables.py
@@ -0,0 +1,31 @@
+# How can we create a variable
+
+# name = 'Farah' # declaring string variable
+# # creating a variable called name to store user name
+# age = 22 # integer
+# # creating a variable called age to store user age
+# hourly_wage = 5 # Integer
+# # creating a variable called hourly_wage to store user hourly wage
+# travel_allowance = 2.4 # Float
+# # Creating a variable called travel_allowance to store user travel allowance.
+
+# Types of variables in python
+# string values, int, float, boolean
+
+# print(travel_allowance)
+# print(age)
+# print(hourly_wage)
+
+# How can we take user data
+name = input('Please enter your name ')
+
+print(name)
+# How can we find out the type of variable
+# we have type method in python that gives us the actual data type
+print(type(name))
+
+# recieve data from users
+
+# display data on command line
+
+# save data in csv file.
