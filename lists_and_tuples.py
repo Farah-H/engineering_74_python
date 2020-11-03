313d75c7b085ab085900f7956f082889e35a67c5
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-03 12:19:00 +0000

    added notes on tuples

--- lists_and_tuples.py
+++ lists_and_tuples.py
@@ -37,26 +37,37 @@
 # add delete replace pop
 
 
-my_mixed_list = [1, 2.4, [1,3,4] , 'potato', 'porridge', 6.7, 10]
+# my_mixed_list = [1, 2.4, [1,3,4] , 'potato', 'porridge', 6.7, 10]
 
-for _ in my_mixed_list:
-    print(_, type(_))
+# for _ in my_mixed_list:
+#     print(_, type(_))
 
-#add
-my_mixed_list.append(600)
-print(my_mixed_list)
+# #add
+# my_mixed_list.append(600)
+# print(my_mixed_list)
 
-#delete
-my_mixed_list.remove('potato')
-print(my_mixed_list)
+# #delete
+# my_mixed_list.remove('potato')
+# print(my_mixed_list)
 
-#replace
-my_mixed_list[1] = 'tomatoes'
-print(my_mixed_list)
+# #replace
+# my_mixed_list[1] = 'tomatoes'
+# print(my_mixed_list)
 
-#pop
-my_mixed_list.pop()
-print(my_mixed_list)
+# #pop
+# my_mixed_list.pop()
+# print(my_mixed_list)
 
-# use indexing to print the list in reverse order 
-print(my_mixed_list[::-1])
+# # use indexing to print the list in reverse order 
+# print(my_mixed_list[::-1])
+
+# Tuples are IMMUTABLE - can't be changed 
+# Use case NI number, DOB, place of birth
+# Sytax: we use commas to declare a tuple, but standard practice is also to encapsulate a tuple in ()
+
+short_list = ('paracetemol', 'eggs', 'supermalt')
+print(type(short_list))
+
+#short_list[1] = 'fruits' 
+
+print(short_list[-1])
