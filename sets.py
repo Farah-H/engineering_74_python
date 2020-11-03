a68a26fa375120fdf3a2e97ca9ce68e9a0396930
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-03 16:19:45 +0000

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,14 @@
+# set - and frozen sets data collection with sets 
+# indexing - THERE IS NO INDEXING IN SETS 
+# sets are unordered 
+# syntax: {} brackets for sets
+
+car_parts = {'wheels', 'doors', 'radio'}
+print(car_parts)
+
+#managing data with sets 
+car_parts.remove('wheels')
+print(car_parts)
+
+car_parts.add('engine')
+print(car_parts)
